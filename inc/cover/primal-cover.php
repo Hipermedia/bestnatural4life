71cec2d435ef1439dcf664deba90e59a1a82c085
cosmo811 <cosmo.op8@gmail.com>
2016-04-29 20:10:25 -0500

    Cover terminado

--- inc/cover/primal-cover.php
+++ inc/cover/primal-cover.php
@@ -6,9 +6,9 @@
 		</figure>
 		<!-- Títulos y llamadas a la acción -->
 		<div class="PrimalCover-titulos">
-			<h1 class="PrimalCover-titulo"><?php the_field('tituloPortada', 'option'); ?></h1>		
-			<h2 class="PrimalCover-subtitulo"><?php the_field('subtituloPortada', 'option'); ?></h2>
-			<a href="<?php the_field('enlacePortada', 'option'); ?>" class="PrimalCover-action"><?php the_field('textoEnlacePortada', 'option'); ?></a>
+			<h2 class="PrimalCover-titulo"><?php the_field('tituloPortada', 'option'); ?></h2>		
+			<div class="PrimalCover-subtitulo"><?php the_field('subtituloPortada', 'option'); ?></div>
+			<a class="PrimalCover-zelda" href="<?php the_field('enlacePortada', 'option'); ?>"><?php the_field('textoEnlacePortada', 'option'); ?></a>
 		</div>
 	</div>		
 </section>
