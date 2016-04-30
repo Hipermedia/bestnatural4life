44129d98d70825c06434a8b49f41ce276324a27c
cosmo811 <cosmo.op8@gmail.com>
2016-04-29 20:32:10 -0500

    Bloque beneficios finalizado

--- inc/blocks/primal-blocks.php
+++ inc/blocks/primal-blocks.php
@@ -8,11 +8,8 @@
 	<div class="PrimalBlocks-contenido u-contenedor">
 		<!-- Títulos de la sección -->
 		<h1 class="PrimalBlocks-titulo"><?php the_field('tituloBeneficiosPortada', 'option'); ?></h1>
-		
 		<?php if( have_rows('beneficiosPortada', 'option') ): ?>
 			<?php while( have_rows('beneficiosPortada', 'option') ): the_row(); ?>
-				
-				<!-- bloque -->
 				<div class="PrimalBlocks-block">
 					<figure class="PrimalBlocks-blockFigure">
 						<img src="<?php the_sub_field('img'); ?>" alt="<?php the_sub_field('titulo'); ?>">
@@ -22,8 +19,8 @@
 						<?php the_sub_field('descripcion'); ?>
 					</p>
 				</div>
-
 			<?php endwhile; ?>
 		<?php endif; ?>
+		<a class="PrimalBlocks-contenidoZelda" href="<?php the_field('enlaceBeneficiosPortada', 'option'); ?>"><?php the_field('textoEnlaceBeneficiosPortada', 'option'); ?></a>
 	</div>
 </section>
