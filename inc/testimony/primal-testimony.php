a1b04f0b88873ac8a931cafe373ebd8916749289
cosmo811 <cosmo.op8@gmail.com>
2016-05-02 12:46:20 -0500

    Testimonios finalizado

--- inc/testimony/primal-testimony.php
+++ inc/testimony/primal-testimony.php
@@ -7,25 +7,23 @@
 	<!-- Contenedor -->
 	<div class="PrimalTestimony-contenido u-contenedor">
 		<!-- Títulos de la sección -->
-		<h2 class="PrimalTestimony-titulo"><?php the_field('tituloTestimoniosPortada', 'option'); ?></h2>
+		<h2 class="PrimalTestimony-titulo u-block-title"><?php the_field('tituloTestimoniosPortada', 'option'); ?></h2>
 		
 		<?php if( have_rows('testimoniosPortada', 'option') ): ?>
 			<?php while( have_rows('testimoniosPortada', 'option') ): the_row(); ?>
-				
-				<!-- bloque -->
 				<div class="PrimalTestimony-block">
 					<figure class="PrimalTestimony-blockFigure">
-						<i class="fa fa-quote-left"></i>
 						<img src="<?php the_sub_field('imagen', 'option'); ?>" alt="">
 					</figure>
-					<blockquote class="PrimalTestimony-blockCita">
-						<?php the_sub_field('cita'); ?>
-					</blockquote>
-					<h4 class="PrimalTestimony-blockAuthor">
-						<?php the_sub_field('autor'); ?>
-					</h4>
+					<div class="PrimalTestimony-blockComment">
+						<blockquote class="PrimalTestimony-blockCita">
+							<?php the_sub_field('cita'); ?>
+						</blockquote>
+						<h4 class="PrimalTestimony-blockAuthor">
+							<?php the_sub_field('autor'); ?>
+						</h4>
+					</div>
 				</div>
-
 			<?php endwhile; ?>
 		<?php endif; ?>
 	</div>
